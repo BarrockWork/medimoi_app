f41c684841ff870fc87985eabee784bd9c010331
SandRatiaray <s.ratiaray@gmail.com>
2022-07-25 14:22:40 +0200

    [KHYS-22] Change Dashboard into Acceuil

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -7,27 +7,27 @@ const icons = { IconDashboard, IconAdjustments };
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 
 const dashboard = {
-    id: 'dashboard',
-    title: 'Dashboard',
-    type: 'group',
-    children: [
-        {
-            id: 'default',
-            title: 'Dashboard',
-            type: 'item',
-            url: '/dashboard/default',
-            icon: icons.IconDashboard,
-            breadcrumbs: false
-        },
-        {
-            id: 'test-api',
-            title: 'Test-api',
-            type: 'item',
-            url: '/test-api',
-            icon: icons.IconAdjustments,
-            breadcrumbs: false
-        }
-    ]
+  id: 'dashboard',
+  title: 'Accueil',
+  type: 'group',
+  children: [
+    {
+      id: 'default',
+      title: 'Accueil',
+      type: 'item',
+      url: '/accueil',
+      icon: icons.IconDashboard,
+      breadcrumbs: false,
+    },
+    {
+      id: 'test-api',
+      title: 'Test-api',
+      type: 'item',
+      url: '/test-api',
+      icon: icons.IconAdjustments,
+      breadcrumbs: false,
+    },
+  ],
 };
 
 export default dashboard;
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -5,57 +5,65 @@ import MainLayout from 'layout/MainLayout';
 import Loadable from 'ui-component/Loadable';
 
 // dashboard routing
-const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
+const DashboardDefault = Loadable(
+  lazy(() => import('views/dashboard/Default'))
+);
 
 // App routing
-const TestApiPage = Loadable(lazy(()=> import('views/test-api-page')))
+const TestApiPage = Loadable(lazy(() => import('views/test-api-page')));
 
 // utilities routing
-const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
+const UtilsTypography = Loadable(
+  lazy(() => import('views/utilities/Typography'))
+);
 const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
 const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
-const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
-const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
+const UtilsMaterialIcons = Loadable(
+  lazy(() => import('views/utilities/MaterialIcons'))
+);
+const UtilsTablerIcons = Loadable(
+  lazy(() => import('views/utilities/TablerIcons'))
+);
 
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-        {
-            path: '/',
-            element: <DashboardDefault />
-        },
-        {
-            path: '/dashboard/default',
-            element: <DashboardDefault />
-        },
-        {
-            path: '/test-api',
-            element: <TestApiPage />
-        },
-        {
-            path: '/utils/util-typography',
-            element: <UtilsTypography />
-        },
-        {
-            path: '/utils/util-color',
-            element: <UtilsColor />
-        },
-        {
-            path: '/utils/util-shadow',
-            element: <UtilsShadow />
-        },
-        {
-            path: '/icons/tabler-icons',
-            element: <UtilsTablerIcons />
-        },
-        {
-            path: '/icons/material-icons',
-            element: <UtilsMaterialIcons />
-        }
-    ]
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <DashboardDefault />,
+    },
+    {
+      path: '/accueil',
+      element: <DashboardDefault />,
+    },
+    {
+      path: '/test-api',
+      element: <TestApiPage />,
+    },
+    {
+      path: '/utils/util-typography',
+      element: <UtilsTypography />,
+    },
+    {
+      path: '/utils/util-color',
+      element: <UtilsColor />,
+    },
+    {
+      path: '/utils/util-shadow',
+      element: <UtilsShadow />,
+    },
+    {
+      path: '/icons/tabler-icons',
+      element: <UtilsTablerIcons />,
+    },
+    {
+      path: '/icons/material-icons',
+      element: <UtilsMaterialIcons />,
+    },
+  ],
 };
 
 export default MainRoutes;
